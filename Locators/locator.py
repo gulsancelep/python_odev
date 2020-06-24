8606676c2817c13ea866607fc0794d4b6e66f38c
gulsan.celep <gulsan.celep@useinsider.com>
2020-06-24 12:16:55 +0300

    Fixlendi...

--- Locators/locator.py
+++ Locators/locator.py
@@ -13,39 +13,39 @@ from selenium.webdriver.common.by import By
 # 1- UTM settings on Newsletter design page
 # $('[class="checkboxDefault checkboxDefault"]').click()
 # $('.checkboxDefault').click()
-Utm_settings = (By.CLASS_NAME, 'checkboxDefault')
+UTM_SETTINGS = (By.CLASS_NAME, 'checkboxDefault')
 
 # 2- on Maven design page, when you rename variation cross and check buttons
 # $('.icon-checked').click()
-icon_checked = (By.CLASS_NAME, 'icon-checked')
+ICON_CHECKED = (By.CLASS_NAME, 'icon-checked')
 
 # $('.variationNameUpdateCancel')[0].click()
-icon_closed = (By.CLASS_NAME, 'variationNameUpdateCancel')
+ICON_CLOSED = (By.CLASS_NAME, 'variationNameUpdateCancel')
 
 # 3- input warning message, for example, on Journey SMS page when we try to save empty title input
-warning_message = (By.LINK_TEXT, 'The Your Message field is required.')
+WARNING_MESSAGE = (By.LINK_TEXT, 'The Your Message field is required.')
 
 # 4-  on Journey do list filter input
-journey_filter_input = (By.CLASS_NAME, 'icon-search')
+JOURNEY_FILTER_INPUT = (By.CLASS_NAME, 'icon-search')
 
 # 5- tags list when we select template in created journey campaign and didn’t click Use this Template button
-tags_list = (By.CLASS_NAME, 'class="btn btnBlue btnSelectTemplate animationHalf"')
+TAGS_LIST = (By.CLASS_NAME, 'class="btn btnBlue btnSelectTemplate animationHalf"')
 
 # 6- on Journey do list how to click exactly needed campaign’s
 # Statistics button if there is more than 1 campaigns in the list
-statistics = (By.XPATH, '//*[@id="personalizationList"]/tbody/tr[1]/td[7]')
+STATISTICS = (By.PARTIAL_LINK_TEXT, "/journey-builder/75758/statistics")
 
 # 7- on Message box design page remove variation cross icon
-remove_variation = (By.CLASS_NAME, 'variationDelete')
+REMOVE_VARIATION = (By.CLASS_NAME, 'variationDelete')
 
 # 8- Remove/ Change element buttons on Journey canvas page
-change_element = (By.XPATH, '//*[@id="wait-for-some-time-2"]/svg/image[1]')
+CHANGE_ELEMENT = (By.PARTIAL_LINK_TEXT, "/assets/img/journey-builder/do-it-your-self/change-element-icon.svg")
 # //*[@id="wait-for-some-time-2"]/svg/image[1]
-remove_element = (By.XPATH, '//*[@id="wait-for-some-time-2"]/svg/image[2]')
+REMOVE_ELEMENT = (By.PARTIAL_LINK_TEXT, "/assets/img/journey-builder/do-it-your-self/delete-element-icon.svg")
 # //*[@id="wait-for-some-time-2"]/svg/image[2]
 
 # 9- Alert toaster on any page of panel
-alert_panel = (By.LINK_TEXT, 'Please fill all necessary fields')
+ALERT_PANEL = (By.LINK_TEXT, 'Please fill all necessary fields')
 
 # 10- on Api and js (access area) free js body input
-alert_api = (By.LINK_TEXT, 'You are not authorized to access this page!')
+ALERT_API = (By.LINK_TEXT, 'You are not authorized to access this page!')
