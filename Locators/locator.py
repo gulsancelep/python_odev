b957c5fff9b191e96680982f7e9a56cd2601969a
gulsan.celep <gulsan.celep@useinsider.com>
2020-11-10 13:05:27 +0300

    Fixlendi...

--- Locators/locator.py
+++ Locators/locator.py
@@ -7,13 +7,13 @@
 # on Message box design page remove variation cross icon
 #  Remove/ Change element buttons on Journey canvas page
 #  Alert toaster on any page of panel
-#  on Api and js (access area) free js body input
+#10- on Social Proof ‘Create New Personalization’
 from selenium.webdriver.common.by import By
 
 # 1- UTM settings on Newsletter design page
-# $('[class="checkboxDefault checkboxDefault"]').click()
-# $('.checkboxDefault').click()
+# $('.checkboxDefault').click() clickleme
 UTM_SETTINGS = (By.CLASS_NAME, 'checkboxDefault')
+UTM_SETTINGS_ON = (By.CLASS_NAME, 'utmSettings')
 
 # 2- on Maven design page, when you rename variation cross and check buttons
 # $('.icon-checked').click()
@@ -23,17 +23,18 @@ ICON_CHECKED = (By.CLASS_NAME, 'icon-checked')
 ICON_CLOSED = (By.CLASS_NAME, 'variationNameUpdateCancel')
 
 # 3- input warning message, for example, on Journey SMS page when we try to save empty title input
-WARNING_MESSAGE = (By.LINK_TEXT, 'The Your Message field is required.')
+WARNING_MESSAGE = (By.CSS_SELECTOR, '.smsMessage > fieldset > p')
 
 # 4-  on Journey do list filter input
-JOURNEY_FILTER_INPUT = (By.CLASS_NAME, 'icon-search')
+JOURNEY_FILTER_INPUT = (By.ID, 'search-value')
 
 # 5- tags list when we select template in created journey campaign and didn’t click Use this Template button
 TAGS_LIST = (By.CLASS_NAME, 'class="btn btnBlue btnSelectTemplate animationHalf"')
 
 # 6- on Journey do list how to click exactly needed campaign’s
 # Statistics button if there is more than 1 campaigns in the list
-STATISTICS = (By.PARTIAL_LINK_TEXT, "/journey-builder/75758/statistics")
+# Hangi istatistiğe tıklamak istersek nth-child(i) buradaki alanı değiştirebiliriz
+STATISTICS = (By.CSS_SELECTOR, " tr:nth-child(3) > td.vuetable-slot.statistics > div > div")
 
 # 7- on Message box design page remove variation cross icon
 REMOVE_VARIATION = (By.CLASS_NAME, 'variationDelete')
@@ -45,7 +46,9 @@ REMOVE_ELEMENT = (By.PARTIAL_LINK_TEXT, "/assets/img/journey-builder/do-it-your-
 # //*[@id="wait-for-some-time-2"]/svg/image[2]
 
 # 9- Alert toaster on any page of panel
-ALERT_PANEL = (By.LINK_TEXT, 'Please fill all necessary fields')
+ALERT_PANEL = (By.CLASS_NAME, 'messageAlertBoxIcon')
+ALERT_PANEL_TEXT = (By.LINK_TEXT, 'Please fill all necessary fields.')
+ALERT = (By.CSS_SELECTOR, 'div.messageAlertBox')
 
-# 10- on Api and js (access area) free js body input
-ALERT_API = (By.LINK_TEXT, 'You are not authorized to access this page!')
+#10- on Social Proof ‘Create New Personalization’
+CREATE_NEW_PERSONALIZATION = (By.CSS_SELECTOR, 'div.personalization > div:nth-child(1) > div')
